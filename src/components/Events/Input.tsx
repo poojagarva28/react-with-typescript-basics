c082833eed9aaa81a3409f01d0a723f5c81ede24
pooja <poojagarva123@gmail.com>
2023-08-09 12:31:09 +0530

    destructuring props

--- src/components/Events/Input.tsx
+++ src/components/Events/Input.tsx
@@ -5,14 +5,14 @@ type InputProps = {
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const Input = (props: InputProps) => {
+export const Input = ({ value, handleChange }: InputProps) => {
   // events with prop and event as function inside component, here commented is as function inside component
   //   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   //     console.log(event.target.value);
   //   };
 
   return (
-    <input type="text" value={props.value} onChange={props.handleChange} />
+    <input type="text" value={value} onChange={handleChange} />
     // onchange as prop and onchange as funciton inside component, here commented is as funciton inside component
     // onChange={handleChange} />
   );
