4e09fc2a6ddb140121a5165901fc84374965653f
pooja <poojagarva123@gmail.com>
2023-08-10 22:32:10 +0530

    useState future value (null or userdata while login and setting the user info to the state

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+type AuthUser = {
+  name: string;
+  email: string;
+};
 
 const User = () => {
-  return <div>User</div>;
+  const [user, setUser] = useState<AuthUser | null>(null);
+
+  const loginhandler = () => {
+    setUser({
+      name: "pooja",
+      email: "poojagarva123@gmail.com",
+    });
+  };
+  const logoutHandler = () => {
+    setUser(null);
+  };
+  return (
+    <>
+      <button onClick={loginhandler}>login</button>
+      <button onClick={logoutHandler}>log out</button>
+      <p>User name: {user?.name}</p>
+      <p>User email: {user?.email}</p>
+    </>
+  );
 };
 
 export default User;
